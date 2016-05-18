de20b4ed2b5a7cb49019912f56eb6274cf022bb1
kahsoonyap <kahsoonyap1998@hotmail.com>
2016-05-17 19:57:04 -0700

    Can find its way to location now. Only works on single path right now.

--- 6.js
+++ 6.js
@@ -1,9 +1,11 @@
+var deadEnd = {room: "0"};
+
 var block = function(roomNum){
     var room = roomNum;
-    var north = 0;
-    var south = 0;
-    var east = 0;
-    var west = 0;
+    var north = deadEnd;
+    var south = deadEnd;
+    var east = deadEnd;
+    var west = deadEnd;
     
     var getName = function(){
 	return room;
@@ -35,7 +37,7 @@ var block = function(roomNum){
     }
     
     return {
-	room: room,
+	room: getName(),
 	north: getNorth,
 	south: getSouth,
 	east: getEast,
@@ -45,6 +47,66 @@ var block = function(roomNum){
 	setEast: setEast,
 	setWest: setWest
     }
+}
+
+var pathFinder = function(start, end){
+    var counter = 0;
+    var direction = "";
+    var currentBlock = start;
+    console.log("path finder");
+    if (start.room < end.room){
+	console.log("lower room");
+	direction = "east";
+	console.log(direction);
+    } else if (start.room > end.room){
+	direction = "west";
+    } else {
+	console.log("something is wrong if we get here");
+    }
+    var walkStraight = function(){
+	counter ++;
+	console.log("walkStraight call");
+	console.log(currentBlock[direction]().room != "0");
+	while (currentBlock[direction]().room != "0"){
+	    console.log("start loop");
+	    console.log(currentBlock[direction]());
+	    if (currentBlock[direction]().room == end.room){
+		console.log("room found");
+		return
+	    }
+	    console.log("restarting loop");
+	    console.log(currentBlock[direction]());
+	    currentBlock = currentBlock[direction]();
+	}
+	console.log("finshed while loop");
+	if (currentBlock.north().room != "0" && direction != "south"){
+	    direction = "north";
+	    currentBlock = currentBlock.north();
+	} else if (currentBlock.south().room != "0" && direction != "north"){
+	    direction = "south";
+	    currentBlock = currentBlock.south();
+	} else if (currentBlock.east().room != "0" && direction != "west"){
+	    direction = "east";
+	    console.log("should be east");
+	    currentBlock = currentBlock.east();
+	} else if (currentBlock.west().room != "0" && direction != "east"){
+	    direction = "west";
+	    currentBlock = currentBlock.west();
+	} else {
+	    console.log("weird dead end");
+	}
+	if (currentBlock.room == end.room){
+	    console.log("room found");
+	    return
+	}
+	console.log(direction);
+	console.log("one cycle");
+	console.log(currentBlock.room);
+	if (counter != 4){
+	    walkStraight();
+	}
+    }
+    walkStraight();
 }
 
 var block1 = block("block1");
@@ -57,12 +119,6 @@ var block7 = block("block7");
 var block8 = block("block8");
 var block9 = block("block9");
 var block10 = block("block10");
-var block11 = block("block11");
-var block12 = block("block12");
-var block13 = block("block13");
-var block14 = block("block14");
-var block15 = block("block15");
-var block16 = block("block16");
 var r601 = block("601");
 var r602 = block("602");
 var r603 = block("603");
@@ -72,27 +128,24 @@ var r606 = block("606");
 var r607 = block("607");
 var r608 = block("608");
 var r609 = block("609");
-var r6010 = block("6010");
-var r6011 = block("6011");
-var r6012 = block("6012");
-var r6013 = block("6013");
-var r6014 = block("6014");
-var r6015 = block("6015");
-var r6016 = block("6016");
+var r640 = block("640");
 
 r601.setSouth(block1);
 block1.setNorth(r601);
+r640.setNorth(block3);
+block3.setSouth(r640);
 
 block1.setEast(block2);
 block2.setEast(block3);
-block3.setEast(block4);
+/*block3.setEast(block4);
 block4.setEast(block5);
 block5.setEast(block6);
 block6.setEast(block7);
 block7.setEast(block8);
 block8.setEast(block9);
 block9.setEast(block10);
-
+*/
+/*
 block10.setWest(block9);
 block9.setWest(block8);
 block8.setWest(block7);
@@ -100,6 +153,7 @@ block7.setWest(block6);
 block6.setWest(block5);
 block5.setWest(block4);
 block4.setWest(block3);
+*/
 block3.setWest(block2);
 block2.setWest(block1);
 
