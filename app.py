7beb2b460d0bcf0f26b4f2dcb75afd294756ceae
hoyinho <hoyinho999@gmail.com>
2016-06-02 23:54:16 -0400

    testing in prototype for directions

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect,url_for, Markup
 from oauth2client.client import flow_from_clientsecrets
 import json
+import query
+
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
@@ -16,6 +18,37 @@ def verified():
 def denied():
     return render_template("index.html", error="You do not have permission!")
 
+@app.route("/coords")
+def coords():
+    roomNum = request.args.get("room")
+    room = query.get_xy(roomNum)
+    return json.dumps(room)
+
+@app.route("/intersect")
+def intersect():
+    roomNum = request.args.get("room")
+    direct = request.args.get("direction")
+    intersection = query.get_intersection(roomNum, direct)
+    return json.dumps(intersection)
+
+@app.route("/stairs")
+def stairs():
+    roomNum = request.args.get("room")
+    staircase = query.get_staircase(roomNum)
+    return json.dumps(staircase)
+
+@app.route("/intersectXY")
+def intersectXY():
+    intersect = request.args.get("intersect")
+    xy = query.get_intersectXY(intersect)
+    return json.dumps(xy)
+
+@app.route("/intersectDirect")
+def intersectDirect():
+    intersect = request.args.get("intersect")
+    direct = request.args.get("direct")
+    direction = query.get_intersectDirect(intersect, direct)
+    return direction
 
 if __name__ == "__main__":
     app.debug=True
--- query.py
+++ query.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-DB_NAME = "map.db"
+DB_NAME = "intersection.db"
 
 def get_xy(room):
     conn = sqlite3.connect(DB_NAME)
@@ -20,6 +20,29 @@ def get_intersection(room, direction):
     conn.close()
     return intersection
 
-def get_staircase(room, direction):
-    conn = sqlite3
-    
+def get_staircase(room):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    q = "SELECT staircase FROM rooms where rooms.room=" + room + ";"
+    staircase = c.execute(q).fetchall()
+    conn.commit()
+    conn.close()
+    return staircase
+
+def get_intersectXY(intersect):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    q = "SELECT x,y FROM intersections where intersections.id=" + intersect + ";"
+    xy = c.execute(q).fetchall()
+    conn.commit()
+    conn.close()
+    return staircase
+
+def get_intersectDirect(intersect, direct):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    q = "SELECT " + direct + " FROM intersections where intersections.id=" + intersect + ";"
+    direction = c.execute(q).fetchall()
+    conn.commit()
+    conn.close()
+    return direction
