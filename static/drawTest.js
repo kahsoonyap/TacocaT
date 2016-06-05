ab7a50bb68bcc65ad08fe812657a9d8d1929bb9e
Javis <wujavis@gmail.com>
2016-06-05 19:31:51 -0400

    fixed array problem. bug showed up

--- static/drawTest.js
+++ static/drawTest.js
@@ -1,3 +1,4 @@
+
 var svg = document.getElementById("map");
 var height = Math.floor(screen.height * .7 / 2) * 2;
 var width = Math.floor (screen.width * .5 / 2) * 2;
@@ -14,13 +15,34 @@ svg.appendChild(map);
 
 var drawPath = function drawPath(source, dest){
     $.get("/drawPath", {source:source, dest:dest}, function(d){
-	for (i = 0; i < d.length - 1; i++){
-	    d = d[1,d.length]
+	holder=[]
+	fixed=d.substring(1,d.length-1);
+	console.log(fixed);
+	while (fixed.indexOf('[')!=-1 || fixed.indexOf(']')!=-1){ 
+	    fixed=fixed.replace("["," ");
+	    fixed=fixed.replace("]"," ");
+	}
+	console.log(fixed);
+	fixed=fixed.split(", ");
+	console.log(fixed);
+	for (i=0;i<fixed.length;i++){
+	    fixed[i]=fixed[i].replace(" ","");
+	}
+	console.log(fixed);
+	path=[]
+	for (i=0;i<fixed.length-1;i++){
+	    holder=[fixed[i],fixed[i+1]];
+	    path.push(holder);
+	}
+	console.log(path);
+		   
+			
+	for (i = 0; i < path.length - 1; i++){
 	    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-	    line.setAttribute("x1", d[i][0] * width);
-	    line.setAttribute("y1", d[i][1] * height);
-	    line.setAttribute("x2", d[i+1][0] * width);
-	    line.setAttribute("y2", d[i+1][1] * height);
+	    line.setAttribute("x1", path[i][0] * width);
+	    line.setAttribute("y1", path[i][1] * height);
+	    line.setAttribute("x2", path[i+1][0] * width);
+	    line.setAttribute("y2", path[i+1][1] * height);
 	    line.setAttribute("stroke", "red");
 	    line.setAttribute("stroke-width", 3);
 	    svg.appendChild(line);
